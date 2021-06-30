25156d26fdcb3ae1d09b13e241afe960462f8e95
Miyamoto Kazuyuki <kazuyuki@nec.com>
2021-06-30 13:47:34 +0900

    Update fio.sh

--- fio.sh
+++ fio.sh
@@ -13,28 +13,15 @@ waiting () {
 	done
 }
 
-echo [d] Random START
-for SIZE in 1K 2K 4K 8K 16K 32K 64K 128K 256K 512K 1M 2M 4M 8M 16M 32M 64M 128M 256M 512M; do
-	clplogcmd -m "$SIZE" 2>&1 > /dev/null
-	echo -n "$SIZE " >> log.txt
-	# Random write
-	fio -filename=/md/f.fio -direct=1 -rw=randwrite -bs=$SIZE -size=8G -runtime=30 -name=job1 | grep "write:" | sed 's/^.*bw=//' | sed "s/KB\/s.*//" | tee -a log.txt
+for METHOD in randwrite write; do
+	echo [d] $METHOD START
+	for SIZE in 1K 2K 4K 8K 16K 32K 64K 128K 256K 512K 1M 2M 4M 8M 16M 32M 64M 128M 256M 512M; do
+		clplogcmd -m "$SIZE" 2>&1 > /dev/null
+		echo -n "$SIZE " >> log.txt
+		fio -filename=/md/f.fio -direct=1 -rw=$METHOD -bs=$SIZE -size=8G -runtime=30 -name=job1 | grep "write:" | sed 's/^.*bw=//' | sed "s/KB\/s.*//" | tee -a log.txt
 
-	# Wait for completion of mirror recovery
-	#waiting; waiting;
-	sleep 5
+		# Wait for completion of mirror recovery
+		waiting; waiting;
+	done
 done
-
-echo [d] Sequential START
-for SIZE in 1K 2K 4K 8K 16K 32K 64K 128K 256K 512K 1M 2M 4M 8M 16M 32M 64M 128M 256M 512M; do
-	clplogcmd -m "$SIZE" 2>&1 > /dev/null
-	echo -n "$SIZE " >> log.txt
-	# Sequential write
-	fio -filename=/md/f.fio -direct=1 -rw=write     -bs=$SIZE -size=8G -runtime=30 -name=job1 | grep "write:" | sed 's/^.*bw=//' | sed "s/KB\/s.*//" | tee -a log.txt
-
-	# Wait for completion of mirror recovery
-	#waiting; waiting;
-	sleep 5
-done
-
 echo [d] END
